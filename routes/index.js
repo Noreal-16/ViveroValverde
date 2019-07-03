d04530d57caf916c6cee3d0ea9f93404b483aa2b
Mina Yukasmina <yukasmina@gmail.com>
2019-07-03 11:13:15 -0500

    fragmentos de vistas

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,16 @@ var persona = require('../modelo/persona')
 var cuenta = require('../modelo/cuenta')
     /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', fragmento: 'login' });
+    res.render('index', { title: 'Express', fragmento: 'banner' });
+});
+router.get('/gallery', function(req, res, next) {
+    res.render('index', { title: 'Express', fragmento: 'articulo' });
+});
+router.get('/service', function(req, res, next) {
+    res.render('index', { title: 'Express', fragmento: 'servicio' });
+});
+router.get('/contacto', function(req, res, next) {
+    res.render('index', { title: 'Express', fragmento: 'contactos' });
 });
 
 /* GET home pagina principal. */
